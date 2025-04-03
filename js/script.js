d1d36beca19150af31b67edc5d28b05fef4e023c
Dariusz Okoński <darek200180@gmail.com>
2025-04-03 14:05:12 +0200

    Remove class active from articles

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 'use strict';
 
-// document.getElementById('test-button').addEventListener('click', function () {
-//   const links = document.querySelectorAll('.titles a');
-//   console.log(links);
-// });
+// NEXT => Magiczne słowo this
 
 const titleClickHandler = function (event) {
-  console.log('Link was clicked!');
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for (const activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
+  for (const activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 

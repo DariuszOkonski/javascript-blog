c56909cb92d37bf25f35a774bab62c3fc8c44bac
Dariusz Okoński <darek200180@gmail.com>
2025-04-04 11:42:02 +0200

    Add functionality to display chosen article in middle column

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
 'use strict';
 
 const titleClickHandler = function (event) {
-  const clickedElement = this;
-  console.log(event);
-  console.log('Link was clicked!')
+  event.preventDefault();
 
+  const clickedElement = this;
+  console.log('Link was clicked!')
+  
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for (const activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
-
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active')
-
+  
   console.log('clickedElement: ', clickedElement)
-
+  
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
   for (const activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
+  
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector('.posts ' + articleSelector)
 
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active')
 };
 
 const links = document.querySelectorAll('.titles a');

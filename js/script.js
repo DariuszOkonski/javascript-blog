d0f2afdc0b746488d26bb955f7a6819111d1bf65
Dariusz Okonski <darek200180@gmail.com>
2025-04-09 13:29:59 +0200

    display tags in right column based on object

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@
   };
 
   const generateTags = function () {
-    let allTags = [];
+    let allTags = {};
     const articles = document.querySelectorAll(optArticleSelector);
 
     for (const article of articles) {
@@ -75,15 +75,22 @@
         const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
         html += linkHTML;
 
-        if(allTags.indexOf(linkHTML) == -1) {
-          allTags.push(linkHTML);
+        if(!allTags[tag]) {
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
         }
       }
       articleTagsSelector.innerHTML = html;
     }
     
     const tagList = document.querySelector(optTagsListSelector);
-    tagList.innerHTML = allTags.join(' ');
+    let allTagsHTML = '';
+    for (const tag in allTags) {
+      allTagsHTML += '<li><a href="#">'+ tag + ' (' + allTags[tag] + ') ' +'</a></li>';
+    }
+
+    tagList.innerHTML = allTagsHTML;
   };
 
   const generateAuthors = function() {
